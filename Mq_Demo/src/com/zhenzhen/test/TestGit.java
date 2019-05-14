746c2bf2692a1a956b224961cbae00c568d0cb32
Freeman <281600944@qq.com>
2019-05-14 20:35:17 +0800

    第三次

--- Mq_Demo/src/com/zhenzhen/test/TestGit.java
+++ Mq_Demo/src/com/zhenzhen/test/TestGit.java
@@ -3,7 +3,8 @@ package com.zhenzhen.test;
 public class TestGit {
 	
 	public static void main(String[] args) {
-		System.out.println("通过git提交到远程仓库22");
+		System.out.println("通过git提交到远程仓库");
+		System.out.println("通过git提交到远程仓库222");
 	}
 
 	
