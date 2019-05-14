cadb4996bbb2d2571dc3ef669aa333b2ea6c3c6f
Administrator <Administrator@PC-20181124TKOD>
2019-05-14 20:24:45 +0800

    提交3

--- Mq_Demo/src/com/zhenzhen/test/TestGit.java
+++ Mq_Demo/src/com/zhenzhen/test/TestGit.java
@@ -3,8 +3,10 @@ package com.zhenzhen.test;
 public class TestGit {
 	
 	public static void main(String[] args) {
-		
 		System.out.println("通过git提交到远程仓库");
+		while(true) {
+			System.out.println("-----草泥马----");
+		}
 	}
 
 }
